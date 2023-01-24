0b6f47574deb3a316d5e0e3a6a857fcb903e7427
Nassim-Belkacem <nass.belkacem7@gmail.com>
2023-01-24 15:57:34 +0100

    Ajout de la clé publique

--- PARTIEL/Definition_Classes.php
+++ PARTIEL/Definition_Classes.php
@@ -50,7 +50,10 @@ class Personne {
 	}
 
 	//Méthodes
-	private function afficher() {
+	private function afficher($this->num . '<br/>';
+	$this->nom . '<br/>';
+	$this->prenom . '<br/>';
+	$this->entreprise->nom . '<br/>';) {
 
 	}
 
